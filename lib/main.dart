a782cdba35442607758051df98bf48061b4efbe3
Dany <danyb-@live.com>
2021-08-03 20:41:09 -0300

    added README.md

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:aog/widgets/input.widget.dart';
+import 'package:aog/widgets/loading-button.widget.dart';
+import 'package:aog/widgets/logo.widget.dart';
+import 'package:aog/widgets/submit-form.dart';
+import 'package:aog/widgets/success.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+import 'pages/home.page.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alcool ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
